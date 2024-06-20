e349f5ee6a1b7bb144fe6d66048ee7185e0888da
akshatMaurya5 <mauryaakshat5@gmail.com>
2024-06-20 15:18:35 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  theme: {
+    colors: {
+      myclr: '#1e293b'
+    }
+  },
+  darkMode: 'class',
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
